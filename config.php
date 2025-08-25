ed9dc82713024a8dce5a2b4dbb311da582d50ac2
Danish2230 <danish.aghamohammadi@gmail.com>
2025-08-25 09:31:04 +0330

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+// config.php
+$host = 'localhost';
+$dbname = 'aala_niroo';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("خطا در اتصال به دیتابیس: " . $e->getMessage());
+}
+?>
